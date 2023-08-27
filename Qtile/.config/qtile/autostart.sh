88683a46e96ac562ceb0888059a63e6b9779b6fd
Markoh Pabellano <markohblpabellano@gmail.com>
2023-08-27 14:39:04 +0800

    reorganize my config files using stow

---
+++ Qtile/.config/qtile/autostart.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+blueman-applet &
+#redshift -l -6.21462:106.84513 &
+
+~/.fehbg &
+#copyq &
+#/usr/bin/emacs --daemon &
+#emacsclient -c -a emacs &
+#megasync &
+#syncthing &
+nm-applet &
+#mictray &
+pasystray &
+#dead-notification-center &
+picom &
+
+#picom --experimental-backends & ##& disown
+/usr/lib/xfce-polkit/xfce-polkit & ##& disown # start polkit agent from xfce
+#xfsettingsd &
+#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & disown # start polkit agent from GNOME
