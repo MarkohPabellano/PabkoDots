1bb83bb83a7618e1cb1258bb85f76750d4c98f9e
Markoh Pabellano <markohblpabellano@gmail.com>
2024-02-11 15:14:33 +0800

    added vimtex

--- Neovim/.config/nvim/init.lua
+++ Neovim/.config/nvim/init.lua
@@ -300,9 +300,27 @@ require('lazy').setup({
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
+{
+  'lervag/vimtex',
+  lazy = false, -- lazy-loading will disable inverse search
+  config = function()
+    vim.api.nvim_create_autocmd({ "FileType" }, {
+      group = vim.api.nvim_create_augroup("lazyvim_vimtex_conceal", { clear = true }),
+      pattern = { "bib", "tex" },
+      callback = function()
+        vim.wo.conceallevel = 2
+      end,
+    })
+
+    vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
+    vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
+    vim.g.vimtex_view_method = 'zathura'
+  end,
+},
+
 -- install without yarn or npm markdown preview
 {
-    "iamcco/markdown-preview.nvim",
+    'iamcco/markdown-preview.nvim',
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
